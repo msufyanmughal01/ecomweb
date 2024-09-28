a20262729293dcb6373f4622cb8bdb0ee6e442d3
M Sufyan Mughal <mskhalid703@gmail.com>
2024-09-28 12:46:15 +0500

    Update route.ts

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest,NextResponse } from "next/server"
-export const Get = async (request:Request)=>{
+// import { NextRequest,NextResponse } from "next/server"
+// export const Get = async (request:Request)=>{
      
-}
+// }
